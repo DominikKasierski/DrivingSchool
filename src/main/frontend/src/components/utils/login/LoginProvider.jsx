47412ba23bd2188e9bbbe218e1e50ae031d82ecc
224323 Kasierski Dominik <224323@edu.p.lodz.pl>
2022-06-12 22:05:42 +0200

    LoginProvider fixed

--- src/main/frontend/src/components/utils/login/LoginProvider.jsx
+++ src/main/frontend/src/components/utils/login/LoginProvider.jsx
@@ -4,6 +4,7 @@ import {
     useCustomNotification, useDangerNotification, useSuccessNotification
 } from "../notifications/NotificationProvider";
 import i18n from "../../../i18n";
+import axios from "axios";
 import {ResponseErrorsHandler} from "../handlers/ResponseErrorsHandler";
 import {notificationDuration, notificationType} from "../notifications/Notification";
 
@@ -19,37 +20,36 @@ export const LoginProvider = ({children}) => {
     const dispatchDangerNotification = useDangerNotification();
     const dispatchSuccessNotification = useSuccessNotification();
 
-    const validateTokenExpirationDate = () => {
-        const storedToken = localStorage.getItem("token");
-        if (storedToken === undefined || storedToken == null) return
-        const decodedToken = jwt_decode(storedToken);
-        const expirationDate = new Date(decodedToken.exp * 1000)
-        if (expirationDate < new Date()) {
-            localStorage.removeItem("token")
-            localStorage.removeItem("username")
-            localStorage.removeItem("currentRole")
-            setToken(null)
-            setUsername('')
-            setCurrentRole('')
-        } else {
-            setToken(storedToken)
-            console.log(storedToken)
-            console.log(token)
-            debugger
-        }
-        setCurrentRole(localStorage.getItem('currentRole'));
-        setUsername(localStorage.getItem('username'));
-        debugger
+    const handleRefreshBox = () => {
+        dispatchCustomNotification({
+            "type": notificationType.WARNING,
+            "duration": notificationDuration.LONG,
+            "title": `${i18n.t('dialog.update.token.title')}`,
+            "message": (<> {i18n.t('dialog.update.token.message')} <span className={"text-primary"} style={{cursor: "pointer"}}
+                                                                         onClick={refreshToken}>{i18n.t('dialog.update.token.refresh')}</span></>)
+        })
     }
 
-    const scheduleTokenRefresh = () => {
-        const storedToken = localStorage.getItem("token");
-        if (storedToken === undefined || storedToken == null) return
-        schedule()
+    const refreshToken = (event) => {
+        event.target.closest(".alert").querySelector(".close").click();
+        if (localStorage.getItem("token")) {
+            axios.post(`/resources/auth/updateToken`, localStorage.getItem("token"), {
+                headers: {
+                    "Authorization": `${localStorage.getItem("token")}`
+                }
+            }).then(response => {
+                saveToken("Bearer " + response.data);
+                clearTimeout(localStorage.getItem("timeoutId2") ?? 0)
+                clearTimeout(localStorage.getItem("timeoutId3") ?? 0)
+                schedule()
+                dispatchSuccessNotification({message: i18n.t('dialog.update.token.success')})
+            }).catch(
+                e => ResponseErrorsHandler(e, dispatchDangerNotification)
+            );
+        }
     }
 
     const schedule = () => {
-        debugger
         const id = setTimeout(() => {
             handleRefreshBox();
         }, new Date(jwt_decode(localStorage.getItem("token")).exp * 1000) - new Date() - REFRESH_TIME);
@@ -64,42 +64,31 @@ export const LoginProvider = ({children}) => {
             setCurrentRole('')
         }, new Date(jwt_decode(localStorage.getItem("token")).exp * 1000) - new Date() + 2000);
         localStorage.setItem("timeoutId3", id3.toString())
-        debugger
     }
 
-    const handleRefreshBox = () => {
-        dispatchCustomNotification({
-            "type": notificationType.WARNING,
-            "duration": notificationDuration.LONG,
-            "title": `${i18n.t('dialog.update.token.title')}`,
-            "message": (<> {i18n.t('dialog.update.token.message')} <span className={"text-primary"} style={{cursor: "pointer"}}
-                                                                           onClick={refreshToken}>{i18n.t('dialog.update.token.refresh')}</span></>)
-        })
-    }
-
-    const refreshToken = (event) => {
-        event.target.closest(".alert").querySelector(".close").click();
-        if (localStorage.getItem("token")) {
-            debugger
-            console.log(localStorage.getItem("token"))
-            console.log(token)
-            const requestOptions = {
-                method: "POST",
-                headers: {
-                    Authorization: token,
-                },
-                body: token,
-            };
-
-            fetch("https://localhost:8181/resources/auth/updateToken", requestOptions)
-                .then(response => {
-                    saveToken("Bearer " + response.data);
-                    clearTimeout(localStorage.getItem("timeoutId2") ?? 0)
-                    clearTimeout(localStorage.getItem("timeoutId3") ?? 0)
-                    schedule()
-                    dispatchSuccessNotification({message: i18n.t('"dialog.update.token.success')})
-                }).catch(e => ResponseErrorsHandler(e, dispatchDangerNotification));
+    const validateTokenExpirationDate = () => {
+        const storedToken = localStorage.getItem("token");
+        if (storedToken === undefined || storedToken == null) return
+        const decodedToken = jwt_decode(storedToken);
+        const expirationDate = new Date(decodedToken.exp * 1000)
+        if (expirationDate < new Date()) {
+            localStorage.removeItem("token")
+            localStorage.removeItem("username")
+            localStorage.removeItem("currentRole")
+            setToken(null)
+            setUsername('')
+            setCurrentRole('')
+        } else {
+            setToken(storedToken)
         }
+        setCurrentRole(localStorage.getItem('currentRole'));
+        setUsername(localStorage.getItem('username'));
+    }
+
+    const scheduleTokenRefresh = () => {
+        const storedToken = localStorage.getItem("token");
+        if (storedToken === undefined || storedToken == null) return
+        schedule()
     }
 
     useEffect(() => {
